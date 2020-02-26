d24e7f133406c76b92f53b2cd69247a43890174d
Luke Mueller <muelll79@hotmail.com>
2020-02-26 00:46:02 -0700

    adds basic style

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 import Header from '../Header/Header';
 
+import './Home.css';
+
 const Home = props => {
   return (
     <section className="Home">
       <Header />
+      <div className="Home__div">
+        <p className="Home__p">Providing a home for Colorado-based charities</p>
+        <p className="Home__p">and building bridges from them to you</p>
+      </div>
     </section>
   )
 };
